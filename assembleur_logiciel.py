05636d60a1f8c46b4bfa8101f37b782269492edb
mineosx29 <nadir.ziani29@gmail.com>
2022-02-02 21:56:51 +0100

    Prise en compte de braz, branz, stop et mise en place de l'ignorance des commentaire et espace

---
+++ assembleur_logiciel.py
@@ -0,0 +1,67 @@
+import re
+import sys
+
+fichier_asm = open(sys.argv[1], "r")
+data = fichier_asm.readlines()
+
+instruction_assembleur = ["add", "sub", "mul", "div", "and", "or", "xor", "shl", "seq", "load", "store", "braz", "branz", "jmp", "stop"]
+
+dictionnaire_inst = {}
+
+for n in range(0, len(instruction_assembleur)):
+    dictionnaire_inst[instruction_assembleur[n]] = n + 1
+
+for i in data:
+    tableau = []
+
+    for p in i:
+        if p == "#":
+            break
+        elif p == "\n":
+            break
+        elif p == " ":
+            continue
+        else:
+            tableau.append(p)
+
+    tableau_virgule = "".join(tableau)
+
+    if tableau_virgule == '':
+        continue
+
+    splitage_tableau = tableau_virgule.split(",")
+    if splitage_tableau[0] == "jmp":
+        inst = "jmp"
+        if splitage_tableau[0] == "jmpr":
+            cmd = 1
+        else:
+            cmd = 0
+
+    if splitage_tableau[0] != "jmp":
+        inst = splitage_tableau[0].rsplit("r", 1)[0]
+        print(inst)
+
+    instr = 0
+
+    if inst == "branz":
+        instr += dictionnaire_inst.get(inst) << 27
+        instr += int(splitage_tableau[0].rsplit("r", 1)[1]) << 22
+
+    elif inst == "braz":
+        instr += dictionnaire_inst.get(inst) << 27
+        instr += int(splitage_tableau[0].rsplit["r", 1]) << 22
+    
+    elif inst == "stop":
+        instr += dictionnaire_inst.get(inst) << 27
+
+
+    print(splitage_tableau)
+
+
+
+    
+       
+print(tableau)
+
+
+    
---
+++ exo1.py
@@ -0,0 +1,37 @@
+# Fichier python permettant de assembler.
+import re
+fichier = open("fichier.txt")
+donnee = fichier.readlines()
+instruction = "[A-Z]{3}"
+reg = "[0-9]"
+
+
+
+
+for i in donnee:
+    liste = i.split(",")
+
+    dico = {}
+    dico["ADD"] = 1
+    dico["SUB"] = 2
+    regex_inst = re.compile(instruction)
+    reg_re = re.compile(reg)
+
+    instruction2 = regex_inst.findall(i)
+    registre = reg_re.findall(i)
+    print(instruction2)
+    print(registre)
+
+    if "R" in liste[1]:
+        reg2 = 0
+    else:
+        reg2 = 1
+
+    instruc = 0
+    instruc +=  dico.get(instruction2[0]) << 27
+    instruc +=  int(registre[0]) << 22
+    instruc +=  reg2 << 21
+    instruc +=  int(registre[1]) << 5
+    instruc +=  int(registre[2])
+
+    print(hex(instruc))
